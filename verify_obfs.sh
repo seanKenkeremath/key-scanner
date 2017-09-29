6c23b319da19637509d285030de3114408dd85bb
seanKenkeremath <sean.kenkeremath@willowtreeapps.com>
2017-09-29 15:19:14 -0400

    verify_obfs print strings found

--- verify_obfs.sh
+++ verify_obfs.sh
@@ -24,10 +24,37 @@ fi
 
 sh ./decompile_android.sh -a "$apk_file" -v
 
+total_strings=0
+strings_found=0
+string_exists=false
+found_strings_list=()
+not_found_strings_list=()
+
 echo "Searching for strings.."
 #Loop through all lines in strings.txt -- new line delimited. Last line always caught regardless of newline
  while read p || [[ -n $p ]]; do
-   grep -or "$p" search
+ 	string_exists=false
+	while read -r line; do
+		string_exists=true
+	    echo "$string_exists STRING FOUND: $line"
+	done < <(grep -or "$p" search)
+	if $string_exists; then
+		found_strings_list+=($p)
+		strings_found=$((strings_found + 1))
+	else
+		not_found_strings_list+=($p)
+	fi
+	total_strings=$((total_strings + 1))
  done < $strings_file
+
+ echo "$strings_found out of $total_strings strings found"
+ echo ""
+ echo "Not Found:"
+ echo ""
+ printf '%s\n' "${not_found_strings_list[@]}"
+ echo ""
+ echo "Found:"
+ echo ""
+ printf '%s\n' "${found_strings_list[@]}"
 
 rm -rf search
